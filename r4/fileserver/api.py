bf1d8f4b42c4e8a8e4f44962246eded5aa977114
robert-wagner <raw141@case.edu>
2017-03-04 18:55:05 -0500

    temp changes

--- r4/fileserver/api.py
+++ r4/fileserver/api.py
@@ -2,7 +2,21 @@ from r4.fileserver import server as router
 from flask import render_template, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 import os
-@router.route('/', methods = ['GET',"POST"])
+@router.route('/' methods = ['GET','POST'])
+def testing_upload():
+    if request.method = 'POST':
+        return redirect(url_for('uploaded_file',username = 'username', bucket = 'bucket'
+                                r4id=request.form['r4id']))
+    return '''
+     <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    '''
+@router.route('/<username>/<bucket>/<r4id>', methods = ['GET','POST'])
 def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
@@ -15,10 +29,10 @@ def upload_file():
             flash('No selected file')
             return redirect(request.url)
         if file:
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(router.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
+            filename = secure_filename(request.form['r4id'])
+            file.save(os.path.join(router.config['UPLOAD_FOLDER'], r4id))
+            return redirect(url_for('uploaded_file',username = request.form['username'], bucket = request.form['bucket']
+                                    r4id=request.form['r4id']))
     return '''
      <!doctype html>
     <title>Upload new File</title>
