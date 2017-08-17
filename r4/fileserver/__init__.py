05cd8b54b3504080e19fc8f715c045b7e09cc0b4
Buck Baskin <mobile.wbaskin@gmail.com>
2017-08-16 23:32:34 -0400

    Revise organization, starting with a new standalone client

--- r4/fileserver/__init__.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, request
-from werkzeug.utils import secure_filename
-import os
-UPLOAD_FOLDER = "/home/robert/Github/r4/r4/fileserver/uploads/"
-server = Flask(__name__)
-server.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-from r4.fileserver import api
